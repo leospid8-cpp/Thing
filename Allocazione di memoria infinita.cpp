f8fe35164731c23b54c245cab6adb2f99a86e272
leospid8-cpp <leospid82@gmail.com>
2024-12-16 00:27:29 +0100

    Add files via upload

---
+++ Allocazione di memoria infinita.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    while (true) {
+        int* p = new int[1000000];  // Allocazione massiccia di memoria
+    }
+    return 0;
+}
---
+++ Overload della CPU.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    while (true) {
+        double result = sin(12345.6789) * cos(98765.4321);
+    }
+    return 0;
+}
